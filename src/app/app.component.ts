fc1a84baedff16633ae4cf6d2bdd0f89f9c5ce50
arthuriuss <girault.johan@gmail.com>
2019-04-07 18:41:45 +0200

    Dernière correction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './postlistitem/postlistitem.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Blog OpenClassroom';
+    title = 'Blog OpenClassroom';
+    posts: Array<Post> = [];
+
+    constructor() {
+        this.posts.push(new Post('Mon premier post', 'Texte descriptif du 1er post.'));
+        this.posts.push(new Post('Mon deuxième post', 'Texte descriptif du 2ème post.'));
+        this.posts.push(new Post('Mon troisième post', 'Texte descriptif du 3ème post.'));
+    }
 }
--- src/app/postlist/postlist.component.ts
+++ src/app/postlist/postlist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Post } from '../postlistitem/postlistitem.component';
 
 @Component({
@@ -8,12 +8,10 @@ import { Post } from '../postlistitem/postlistitem.component';
 })
 export class PostlistComponent implements OnInit {
 
-    posts: Array<Post> = [];
+  @Input() posts: Array<Post>;
 
   constructor() {
-      this.posts.push(new Post('Mon premier post', 'Texte descriptif du 1er post.'));
-      this.posts.push(new Post('Mon deuxième post', 'Texte descriptif du 2ème post.'));
-      this.posts.push(new Post('Mon troisième post', 'Texte descriptif du 3ème post.'));
+
   }
 
   ngOnInit() {
